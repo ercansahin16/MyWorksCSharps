e660e06af66a48e2bea95a706afa502c7fb278a6
ercansahin16 <esahin.ast@gmail.com>
2024-10-02 11:29:29 +0300

    first commit

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+   public class Program
+   {
+      static void Main(string[] args)
+      {
+         #region Yazdırma Komutları
+
+
+         //Console.WriteLine("Ercan Şahin");
+         //Console.WriteLine("***** Yemek Kategorileri *****");
+         //Console.WriteLine();
+         //Console.WriteLine("1 - Çorbalar");
+         //Console.WriteLine("2 - Ana Yemekler");
+         //Console.WriteLine("3 - Soğuk Mezeler");
+         //Console.WriteLine("4 - Salatalar");
+         //Console.WriteLine("5 - Tatlılar");
+         //Console.WriteLine("6 - İçecekler");
+         //Console.WriteLine();
+         //Console.WriteLine("***** Yemek Kategorileri *****");
+         #endregion
+
+         #region String Değişkenler
+
+         //string name;
+         //name = "Ercan";
+         //Console.WriteLine(name);
+
+
+         //string customerName;
+         //string customerSurname;
+         //string customerPhone;
+         //string customerEmail, district, city;
+         //customerName = "Ercan";
+         //customerSurname = "Şahin";
+         //customerPhone = "05340136773";
+         //customerEmail = "esahin.ast@gmail.com";
+         //district = "Doğubeyazıt";
+         //city = "Ağrı";
+         //   Console.WriteLine();
+         //   Console.WriteLine($"Müşteri: {customerName} {customerSurname}");
+         //   Console.WriteLine($"{customerPhone} ");
+         //   Console.WriteLine($"{customerEmail} ");
+         //   Console.WriteLine($"{district} ");
+         //   Console.WriteLine($"{city} ");
+
+         #endregion
+
+         #region İnt Değişkenler
+         //int nu = 24;
+         //   Console.WriteLine(nu);
+         //int Ham = 300;
+         //int coka = 25;
+         //   Console.WriteLine("Toplam Sipariş Tutarı : "+(Ham+coka));
+
+
+         #endregion
+
+
+         Console.ReadLine();  
+      }
+   }
+}
---
+++ 02_Variables/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variables
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.OutputEncoding = Encoding.UTF8;
+         #region Double Değişkenler
+
+         Console.WriteLine("******************");
+
+         //double apple = 14.5, orange = 20.9, strawbery = 15.7, potato = 5.99, tomato = 25.25;
+
+         //Console.WriteLine("---- Elma Birim Fiyatı : " + apple + " ₺");
+         //Console.WriteLine("---- Çilek Birim Fiyatı : " + strawbery + " Tl");
+         //Console.WriteLine("---- Patates Birim Fiyatı : " + potato + " Tl");
+         //Console.WriteLine("---- Domates Birim Fiyatı : " + tomato + " Tl");
+         //Console.WriteLine("******************");
+         //double appleGram = 1.25, orangeGram = 2.9, strawberyGram = 1.7, potatoGram = 5.99, tomatoGram = 2.25;
+
+         //Console.WriteLine("---- Toplam Elma Fiyatı : " + (apple * appleGram) + " ₺");
+         //Console.WriteLine("---- Toplam Çilek  Fiyatı : " + (strawbery * strawberyGram) + " Tl");
+         //Console.WriteLine("---- Toplam Patates  Fiyatı : " + (potato * potatoGram) + " Tl");
+         //Console.WriteLine("---- Toplam Domates  Fiyatı : " + (tomato * tomatoGram) + " Tl");
+         //Console.WriteLine("******************");
+         //double totalprice = (apple * appleGram) + (strawbery * strawberyGram) + (potato * potatoGram) + (tomato * tomatoGram);
+         //Console.WriteLine("Toplam Tutar : " + (totalprice));
+
+
+
+
+         #endregion
+
+         #region Char Değişkenler
+
+         // string[] chars = { "Ali", "Ercan" };
+
+         //for (int i = 0; i < chars.Length; i++) 
+         // {
+         //    for (int j = 0; j < chars[i].Length; j++) 
+         //    {
+         //       Console.Write(chars[i][j]);
+         //    }
+         // }
+
+         #endregion
+
+         #region Klavyeden Veri Girişi
+         //string passengerName, passengerSurname,passengerCinsiyet,passengerage;
+         //   Console.Write("Yolcu Adı : ");
+         //passengerName = Console.ReadLine();
+         //Console.Write("Yolcu Soyadı : ");
+         //passengerSurname = Console.ReadLine();
+         //Console.Write("Yolcu Cinsiyet  (E / K tuşlayınız) :");
+         //passengerCinsiyet = Console.ReadLine().ToLower();
+
+         //if (passengerCinsiyet=="e")
+         //{
+         //   Console.WriteLine("Merhaba " + passengerName.ToUpper() + " BEY");
+         //       Console.Write("Lütfen yaşınızı Giriniz : ");
+         //   passengerage = Console.ReadLine();
+         //}
+         //else
+         //{
+         //   Console.WriteLine("Merhaba " + passengerName.ToUpper() + " HANIM");
+         //   Console.Write("Lütfen yaşınızı Giriniz : ");
+         //   passengerage = Console.ReadLine();
+         //}
+         //Console.WriteLine("***-------------------------***");
+         //Console.WriteLine("Aşağıdaki Bilgiler Doğrultusunda Bilet İşlemi gerçekleşmiştir.");
+         //Console.WriteLine($"Adınız : {passengerName.ToUpper()}");
+         //Console.WriteLine($"Soyadınız : {passengerSurname.ToUpper()}");
+         //Console.WriteLine($"Yaşınız : {passengerage}");
+
+         #endregion
+
+         #region İnt Girişleri ve Dönüşümleri
+         //int age;
+         //   Console.Write("Lütfen Yaşınızı Giriniz :");
+         //age =int.Parse( Console.ReadLine());
+         //   Console.WriteLine(age);
+         #endregion
+
+          #region Klavyeden ondalıklı sayi işlemleri
+         //Console.Write("Lütfen kelime giriniz: ");
+         //string kelime = Console.ReadLine().ToLower();
+         //string number = "";
+         //for (int i = 0; i < kelime.Length; i++)
+         //{
+            
+         //   char charss = kelime[i];
+
+         //   if (charss == 'a')
+         //   {
+         //      number = number + 1;
+         //   }
+         //   else if (charss == 'b')
+         //   {
+         //      number = number + 2;
+         //   }
+         //   else if (charss == 'c')
+         //   {
+         //      number = number + 3;
+         //   }
+         //   else if (charss == 'd')
+         //   {
+         //      number = number + 4;
+         //   }
+         //}
+         //   Console.WriteLine(number);
+
+         #endregion
+         Console.ReadLine();
+      }
+   }
+}
